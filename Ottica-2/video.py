b335f489620e7fa2fd42750b66ae7c9d03b911d3
Fracesco215 <francesco215@live.it>
2019-04-12 17:53:38 +0200

    asd

--- Ottica-2/video.py
+++ Ottica-2/video.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from scipy.ndimage.measurements import center_of_mass as cm
+from scipy.signal import savgol_filter as sf
 
+def distanzaMedia(immagine,media):
+	Min=immagine.min()
+	d=0
+	immagine=immagine-Min
+	for i in range(len(immagine)):
+		for j in range(len(immagine[0])):
+			d+=np.sqrt(immagine[i][j]*((media[1]-i)**2+(media[0]-j)**2))
+	return d/immagine.sum()
 
-img=mpimg.imread('dati/fotogrammi/4.jpg')
-imgplot=plt.imshow(img)
-lum_img=img[:,:,0]
+f=open('dati/distanza_media.txt','w+')
+cm1=np.transpose(np.genfromtxt('dati/centro_di_massa.txt',unpack='True'))
+for i in range(5):
+	img=mpimg.imread('dati/fotogrammi/'+str(i)+'.jpg')[:,:,0]
+	d=distanzaMedia(img,cm1[i])
+	print(d,file=f)
+	print(i)
+	plt.imshow(img)
+	plt.errorbar(cm1[i][1],cm1[i][0],yerr=d,xerr=d,color='purple')
+	plt.savefig('immagini/video/'+str(i)+'.png')
+	plt.close()
+f.close()
